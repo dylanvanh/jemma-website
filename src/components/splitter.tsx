c192878b32f9ec5af0e10fb33edbd160bd442316
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2023-12-24 16:05:03 +0200

    add splitter component

---
+++ src/components/splitter.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface SplitterProps extends React.ComponentPropsWithoutRef<"div"> {
+  sectionTitle: string;
+}
+
+const Splitter: React.FC<SplitterProps> = ({
+  sectionTitle,
+  className,
+  ...props
+}) => {
+  return (
+    <div className={cn("my-4", className)} {...props}>
+      <hr className="border-t border-gray-300" />
+      <h2 className="mt-3 px-2 text-lg font-semibold">{sectionTitle}</h2>
+    </div>
+  );
+};
+
+export default Splitter;
