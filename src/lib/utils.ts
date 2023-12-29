c88af0c50b96f0281f028b5a5a1f5536473c000b
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2023-12-29 23:36:50 +0200

    util for determining screen size

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { ScreenSize } from "./constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getIsScreenMobile(screenWidth: number) {
+  return screenWidth <= (ScreenSize.md as number) ? true : false;
 }
