e7602c065cc0bde2506323cb3e886b66809d0e5a
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2024-01-08 11:40:53 +0200

    add name field to contact form

--- src/sections/contact-section.tsx
+++ src/sections/contact-section.tsx
@@ -19,6 +19,14 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 export const contactFormSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "Required",
+    })
+    .max(100, {
+      message: "Message must not be longer than 50 characters.",
+    }),
   email: z.string().email(),
   message: z
     .string()
@@ -36,6 +44,7 @@ export function ContactSection() {
   const form = useForm<z.infer<typeof contactFormSchema>>({
     resolver: zodResolver(contactFormSchema),
     defaultValues: {
+      name: "",
       email: "",
       message: "",
     },
@@ -84,6 +93,23 @@ export function ContactSection() {
             onSubmit={form.handleSubmit(onSubmit)}
             className="w-full space-y-8 text-center"
           >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="mx-auto w-full xl:w-2/3"
+                      placeholder="Your name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
